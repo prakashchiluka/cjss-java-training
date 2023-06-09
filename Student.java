005135c5b1bcdbde61f8593c4a7761a6010fa63c
prakashchiluka <136047510+prakashchiluka@users.noreply.github.com>
2023-06-09 09:56:25 +0530

    Add files via upload
    second commit

--- Student.java
+++ Student.java
@@ -4,4 +4,11 @@ public class Student {
 	private String name;
 	private String branch;
 	private String year;
+	public Student(String name, String branch, String year) {
+		super();
+		this.name = name;
+		this.branch = branch;
+		this.year = year;
+	}
+	
 }
