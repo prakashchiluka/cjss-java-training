99a39cdfb876659387aa1f077e64812282c32d8a
LAPTOP-2L15C3CE\HP-DU1034 <prakash.chiluka@cjsstechnologies.com>
2023-06-09 10:08:25 +0530

    added a name

--- Student.java
+++ Student.java
@@ -4,11 +4,13 @@ public class Student {
 	private String name;
 	private String branch;
 	private String year;
+	private String names;
 	public Student(String name, String branch, String year) {
 		super();
 		this.name = name;
 		this.branch = branch;
 		this.year = year;
 	}
+	
 	
 }
